b6fbf823857a18e0321efcec2d038c96982aa88b
bolognini <matheusbolognini79@gmail.com>
2021-01-24 12:28:17 -0300

    feat: add themes

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -8,4 +8,24 @@ const theme = {
   }
 }
 
+export const lightTheme = {
+  colors: {
+    black: '#121214',
+    white: '#F2F2F1',
+    grey: '#828282',
+    primary: '#F9D078',
+    secondary: '#B52931'
+  }
+}
+
+export const darkTheme = {
+  colors: {
+    black: '#F2F2F1',
+    white: '#121214',
+    grey: '#828282',
+    primary: '#B52931',
+    secondary: '#F9D078'
+  }
+}
+
 export default theme
