d75aaf17f5c079ce6c226e7171ff7e4250968cde
bolognini <matheusbolognini79@gmail.com>
2021-05-24 14:59:12 -0300

    fix: update responsive fallback

--- src/components/ResponsiveFallback/ResponsiveFallback.tsx
+++ src/components/ResponsiveFallback/ResponsiveFallback.tsx
@@ -5,14 +5,13 @@ export const ResponsiveFallback: React.FC = () => (
     <img src="/logo.png" alt="Tormenta20 Logo" />
     <h2>eita! :(</h2>
     <p>
-      Tanto o formulário Helladarion quanto a ficha virtual,&nbsp;
-      <a href="https://helladarion-codex.netlify.app/">Helladarion Codex</a>,
-      foram desenvolvidos para funcionarem apenas em telas maiores.
+      Essa aplicação foi desenvolvida para funcionar apenas em telas maiores.
     </p>
     <p>
-      <b>Talvez</b> no futuro seja adicionado responsividade nas duas
-      aplicações. Por enquanto é recomendado usar um dispositivo com tela maior
-      do que a de um celular.
+      <b>Talvez</b> no futuro seja adicionado responsividade. Por enquanto é
+      recomendado usar um dispositivo com tela maior do que a de um celular. Se
+      você estiver no computador e mesmo assim não consegue ver a página, tente
+      ajustar o zoom.
     </p>
     <p>
       Qualquer dúvida ou sugestão em relação a isso ou outro assunto relacionado
